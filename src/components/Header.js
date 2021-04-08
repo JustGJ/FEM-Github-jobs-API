d9c6c5c9987f184e1e6ce35e9d7e751cc843fa9d
Jeff <jf.gasp@gmail.com>
2021-04-08 16:32:17 +1100

    header desktop ok

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Sun from '../assets/desktop/icon-sun.svg';
+import Moon from '../assets/desktop/icon-moon.svg';
+
+const Header = () => {
+
+    return (
+        <>
+            <div className="header" >
+                <p className="header__title">devjobs</p>
+                <div className="header__darkMode">
+                    <img src={Sun} alt={Sun} />
+                    <div>
+                        <input type="checkbox" className="checkbox" id="dark-mode" />
+                        <label htmlFor="dark-mode" className="label">
+                            <div className="ball"></div>
+                        </label>
+                    </div>
+                   
+                    <img src={Moon} alt={Moon}/>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
