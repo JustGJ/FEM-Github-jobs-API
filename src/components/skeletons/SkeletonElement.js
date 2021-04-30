4fbc1441c7f2a1ac40889639566b90d671b57cd5
Jeff <jf.gasp@gmail.com>
2021-04-30 13:38:19 +1100

    filter reset ok

--- src/components/skeletons/SkeletonElement.js
+++ src/components/skeletons/SkeletonElement.js
@@ -2,10 +2,10 @@
  
  const SkeletonElement = ( {type, typeLoader} ) => {
 
-    const classes = `wave skeleton__${typeLoader}__${type}`;
+    const classes = `skeleton__${typeLoader}__${type}`;
 
      return (
-         <div style={{position: "relative", overflow:'hidden'} } className={classes}></div>
+         <div className={classes}></div>
      );
  };
  
